7d26822a288d510e55789185f80fa10a5082e9d3
ilyalesunov <coolbiird@gmail.com>
2022-11-14 19:11:46 +0300

    ПРОТОТИП КАРТОЧКИ

---
+++ src/components/Universal/Cards/MainCard.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="main-card">
+    <div class="main-card-slider">
+      TODO SWIPER SLIDER
+    </div>
+    <div class="main-card-tags">
+      <p class="main-card-tags-tag" v-for="(tag, index) in testObject" :key="tag.id">{{tag.tag}}</p>
+    </div>
+    <div class="main-card-name">
+      Пропала собака, Москва
+    </div>
+    <div class="main-card-text">
+      Кобель такса кролик длинношёрстный рыжий кличка Ян с ошейником. В руки не даётся чужим. Полтора года.
+    </div>
+    <button class="main-card-btn">Показать на карте</button>
+  </div>
+</template>
+
+<script>
+import { reactive } from 'vue'
+export default {
+  name: "MainCard",
+  setup () {
+    const testObject = reactive([
+      { id: 0, tag: 'Кошка' },
+      { id: 1, tag: 'Казань' }
+    ])
+    return { testObject }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../../assets/scss/utilities/_mixins.scss';
+// ВРЕМЕННЫЕ СТИЛИ, ДЛЯ ПОНИМАНИЯ
+.main-card {
+  min-height: 200px;
+  border: 1px solid;
+  padding: 20px;
+  &-slider {
+    @include flexContainer(row, center,center);
+    height: 100px;
+    width: 100%;
+  }
+  &-tags {
+    @include flexContainer(row, flex-start,center);
+    flex-wrap: wrap;
+    gap: 5px;
+    margin: 0 0 10px 0;
+    &-tag {
+      border: 1px solid red;
+      padding: 2px 10px;
+      border-radius: 7px;
+    }
+  }
+  &-name {
+    margin: 0 0 10px 0;
+    font-size: 20px;
+  }
+  &-text {
+    font-size: 16px;
+    margin: 0 0 20px 0;
+    color: #423d33;
+  }
+  &-btn {
+    @include button(10px 0, green, none, 10px);
+    text-align: center;
+    width: 100%;
+    cursor: pointer;
+    color: white;
+  }
+}
+
+</style>
--- src/components/VHeader.vue
+++ src/components/VHeader.vue
@@ -4,31 +4,48 @@ import VButton from '@/components/VButton.vue';
 
 <template>
   <header class="header">
-    <div class="header-logo">
-      <img class="header-img" src="/logo.png" alt="logo" />
+    <div class="container">
+      <div class="header-wrapper">
+        <img class="header-img" src="/logo.png" alt="logo" />
+        <div class="header-right">
+          <input class="header-input" type="text" placeholder="Номер объявления"/>
+          <router-link class="header-request">ВСЕ ОБЬЯВЛЕНИЯ</router-link>
+          <router-link class="header-request2">ПОДАТЬ ОБЬЯВЛЕНИЕ</router-link>
+          <router-link class="header-registration">ВХОД</router-link>
+        </div>
+      </div>
     </div>
-    <div class="header-right">
-      <input class="header-input" type="text" placeholder="Номер объявления"/>
-      <router-link class="header-request">ВСЕ ОБЬЯВЛЕНИЯ</router-link>
-      <router-link class="header-request2">ПОДАТЬ ОБЬЯВЛЕНИЕ</router-link>
-      <router-link class="header-registration">ВХОД</router-link>
-    </div>
+<!--    <div class="header-logo">-->
+<!--      <img class="header-img" src="/logo.png" alt="logo" />-->
+<!--    </div>-->
+<!--    <div class="header-right">-->
+<!--      <input class="header-input" type="text" placeholder="Номер объявления"/>-->
+<!--      <router-link class="header-request">ВСЕ ОБЬЯВЛЕНИЯ</router-link>-->
+<!--      <router-link class="header-request2">ПОДАТЬ ОБЬЯВЛЕНИЕ</router-link>-->
+<!--      <router-link class="header-registration">ВХОД</router-link>-->
+<!--    </div>-->
   </header>
 </template>
 
 <style lang="scss" scoped>
+@import '@/assets/scss/utilities/_mixins.scss';
 .header {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+  //display: flex;
+  //align-items: center;
+  //justify-content: space-between;
   height: 80px;
-  padding: 20px;
-  margin: 0 20px;
+  padding: 20px 0;
+  //margin: 0 20px;
+  margin: 0 0 40px 0;
   border-bottom: 1px solid #000000;
   &-request, &-request2, &-registration {
+    cursor: pointer;
     &:hover {
       text-decoration: underline;
     }
+  }
+  &-wrapper {
+    @include flexContainer(row, space-between, center)
   }
   &-input {
     margin-right: 40px;
