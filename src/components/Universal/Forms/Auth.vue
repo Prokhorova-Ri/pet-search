84cd48525340ece91c4271ee3ccb52f843f21a43
Prokhorova_Ri <rena2652@gmail.com>
2022-12-09 18:27:24 +0300

    Сбор данных с auth

--- src/components/Universal/Forms/Auth.vue
+++ src/components/Universal/Forms/Auth.vue
@@ -1,38 +1,57 @@
 <template>
   <div class="form-auth">
-    <h2 class="form-auth__title">Авторизация</h2>
-    <form class="form-auth__layout">
-      <input class="form-auth__input" type="text" placeholder="Email" />
-      <input class="form-auth__input" type="text" placeholder="Пароль" />
-      <button :class="button.className">{{ button.name }}</button>
-    </form>
-    <form class="form-auth__regist">
-      <h5>Зарегистрировать аккаунт</h5>
-      <button :class="button.className">{{ button.name }}</button>
-    </form>
+    <div>
+      <h2 class="form-auth__title">Авторизация</h2>
+      <form 
+        @submit.prevent="getParamsFormAuth"
+        class="form-auth__layout">
+        <input v-model="authValues.email" class="form-auth__input" type="text" placeholder="Email" />
+        <input v-model="authValues.password" class="form-auth__input" type="text" placeholder="Пароль" />
+        <Button type="auth" select="submit" />
+      </form>
+    </div>
+    <div class="form-auth__registration">
+      <h5 class="form-auth__reg">Зарегистрировать аккаунт</h5>
+      <Button type="reg" @clickOnButton="changeForm"/>
+    </div>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
-import { getButton } from "@/utilites/dicts/buttons.js"
+import { reactive } from '@vue/reactivity';
+import Button from "../../../components/Universal/Button.vue";
 export default {
   name: "Auth",
-  setup () {
-    const button = ref();
-    button.value = getButton('enter', 'reg');
-    return {
-      button
+  components: {
+    Button,
+  },
+  setup() {
+    
+    const authValues = reactive({
+      email: '',
+      password: ''
+    })
+
+    const changeForm = (code) => {
+      console.log('changeForm', code)
     }
-  }
-}
+
+    const getParamsFormAuth = () => {
+      console.warn('getParamsFormAuth', authValues)
+      //  TODO API 
+    }
+
+    return { changeForm, authValues, getParamsFormAuth }
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-@import './src/assets/scss/utilities/mixins';
-@import './src/assets/scss/utilities/variables';
+@import "./src/assets/scss/utilities/mixins";
+@import "./src/assets/scss/utilities/variables";
 
 .form-auth {
+  @include flexContainer(column, space-between, center);
   background: #fcf8ed;
   text-align: center;
   padding: 40px 20px;
@@ -41,20 +60,21 @@ export default {
   border: 2px solid $grey;
   border-radius: 20px;
   &__title {
-    margin-bottom: 20px;
+    margin-bottom: 30px;
   }
   &__input {
-  margin-top: 20px;
-  width: 60%;
-  border: 1px solid $grey;
-  border-radius: 10px;
-  padding: 15px;
-  color: #b6b6b6;
-  font-size: 14px;
+    margin: 0 0 10px 0;
+    width: 60%;
+    border: 1px solid $grey;
+    border-radius: 10px;
+    padding: 10px ;
+    color: #b6b6b6;
+    font-size: 14px;
   }
-  &__regist{
-    margin-top: 20px;
+  &__registration {
+    width: 60%;
+    border-top: 1px solid $grey;
+    padding: 33px 0 0 0;
   }
 }
-
 </style>
