db1765837d575429ddfaeba4e88aa4ca263884a6
Prokhorova_Ri <rena2652@gmail.com>
2022-12-11 17:36:32 +0300

    style-form

--- src/components/Slider/index.vue
+++ src/components/Slider/index.vue
@@ -16,25 +16,17 @@
       class="mySwiper"
     >
       <swiper-slide>
-        <img
-          class="swiper-image"
-          src="https://sun9-34.userapi.com/impg/1Ewmg_Cqkuy17uRwIAEe2s-3NvUcT6yEXvCaKg/2ZGdAF-rpRA.jpg?size=1200x1600&quality=95&sign=c19f77bd6ccdac0635ca00f178c064f4&type=album"
-          alt=""
-        />
-      </swiper-slide>
-      <swiper-slide>
-        <img
-          class="swiper-image"
-          src="https://sun9-74.userapi.com/impg/DudLP-BnM5EHJqbdT4BVWLKgsyiR8VNfn3oLbg/6H9SYgPBWFM.jpg?size=1440x1920&quality=95&sign=9405545b2c1e7e5f50fd8fdab701b3a0&type=album"
-          alt=""
-        />
-      </swiper-slide>
-      <swiper-slide>
-        <img
-          class="swiper-image"
-          src="https://sun9-7.userapi.com/impg/xlmoR4867Nq4umGWvZSoCEwrtwje08-i-pvZeg/dq3CsOH9WRk.jpg?size=1200x1600&quality=95&sign=6f89c0b3860889c4e52b909ce3ca501f&type=album"
-          alt=""
-        />
+        <div
+            class="swiper-image"
+            style="background-image: url('https://cs8.pikabu.ru/post_img/big/2016/03/06/9/1457279582133433203.jpg')"
+        >
+
+        </div>
+<!--        <img-->
+<!--          class="swiper-image"-->
+<!--          src=""-->
+<!--          alt=""-->
+<!--        />-->
       </swiper-slide>
     </swiper>
   </div>
@@ -83,8 +75,12 @@ export default {
 }
 
 .swiper-image {
-  //height: 250px;
+  height: 250px;
   width: 100%;
-  object-fit: contain;
+  background-position: center;
+  background-size: cover;
+  border-top-left-radius: 45px;
+  border-top-right-radius: 45px;
+
 }
 </style>
--- src/components/Universal/Cards/MainCard.vue
+++ src/components/Universal/Cards/MainCard.vue
@@ -3,21 +3,23 @@
     <div class="main-card-slider">
       <Slider />
     </div>
-    <div class="main-card-tags">
-      <p class="main-card-tags-tag" v-for="tag in testObject" :key="tag.id">{{ tag.tag }}</p>
-    </div>
-    <div class="main-card-name">Пропала собака, Москва</div>
-    <div class="main-card-text">
-      Кобель такса кролик длинношёрстный рыжий кличка Ян с ошейником. В руки не даётся чужим. Полтора года.
-    </div>
-    <Button type="map"/>
-    <div class="main-card-map">
-      <Map
-          border-radius="20px"
-          :coordinates="[59.892315, 30.470127]"
-          :zoom="120"
-          :id="`main-${index}`"
-      />
+    <div class="main-card-block">
+      <div class="main-card-tags">
+        <p class="main-card-tags-tag" v-for="tag in testObject" :key="tag.id">{{ tag.tag }}</p>
+      </div>
+      <div class="main-card-name">Пропала собака, Москва</div>
+      <div class="main-card-text">
+        Кобель такса кролик длинношёрстный рыжий кличка Ян с ошейником. В руки не даётся чужим. Полтора года.
+      </div>
+      <Button type="map"/>
+      <div class="main-card-map">
+        <Map
+            border-radius="20px"
+            :coordinates="[59.892315, 30.470127]"
+            :zoom="120"
+            :id="`main-${index}`"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -52,10 +54,16 @@ export default {
 // ВРЕМЕННЫЕ СТИЛИ, ДЛЯ ПОНИМАНИЯ
 .main-card {
   @include fontFamily($font-family-manrope-500, 16px);
-  background: $dark-gray;
-  color: $fff;
+  background: $fff;
+  color: $dark-gray;
+  border-radius: 45px;
+  //border: 2px solid $border;
+  box-shadow: 9px 11px 0.5rem rgb(164 164 164 / 12%);
   min-height: 200px;
-  padding: 5px;
+  &-block {
+    text-align: center;
+    padding: 10px;
+  }
   &-slider {
     margin: 0 0 15px 0;
   }
@@ -65,8 +73,10 @@ export default {
     gap: 5px;
     margin: 0 0 10px 0;
     &-tag {
-      padding: 6px 10px;
-      background: #d1d1d1;
+      padding: 5px 10px;
+      background: #fcf5e3;
+      border-radius: 25px;
+      border: 2px solid $border;
       @include fontFamily($font-family-manrope-500, 16px);
     }
   }
@@ -77,7 +87,7 @@ export default {
   &-text {
     font-size: 14px;
     margin: 0 0 20px 0;
-    color: #e2e2e2;
+    color: $dark-gray;
   }
   &-map {
     width: 100%;
