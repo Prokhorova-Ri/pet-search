16d0bbbb8a64aa05775f727fcf8b621a5578d67c
Prokhorova_Ri <rena2652@gmail.com>
2023-01-10 21:05:23 +0300

    test

--- src/router.js
+++ src/router.js
@@ -6,13 +6,13 @@ import dev_ilya from "./pages/dev_ilya.vue";
 import doc from "./pages/doc.vue";
 
 
-const isAuthorized = localStorage.hasOwnProperty('token')
+// const isAuthorized = localStorage.hasOwnProperty('token')
 
-const authGuard = function (to, from, next) {
-    console.warn('isAuthorized', isAuthorized)
-    if (!isAuthorized) next({ name: 'Home' })
-    else next()
-}
+// const authGuard = function (to, from, next) {
+//     console.warn('isAuthorized', isAuthorized)
+//     if (!isAuthorized) next({ name: 'Home' })
+//     else next()
+// }
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,7 +28,7 @@ const router = createRouter({
         name: "Profile",
         meta: { layout: 'profile-layout' },
         component: Profile,
-        beforeEnter: authGuard
+        // beforeEnter: authGuard
       },
       //Временная страница для разработки компонентов
       {
